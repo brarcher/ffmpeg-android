dbb55bfe92699629e4e6621234874768290e7aa7
test <test@test.test>
2018-08-28 22:28:38 +0200

    Add patch for successfully enabling libfontconfig and libfreetype in ffmpeg configure

--- android_build.sh
+++ android_build.sh
@@ -10,6 +10,11 @@ if [ $? -eq 0 ]; then
   patch -p0 -f fontconfig/src/fcxml.c < android_donot_use_lconv.patch
 fi
 
+patch  -p0 -N --dry-run --silent -f ffmpeg/configure < ffmpeg_add_zlib_to_fontconfig_and_freetype.patch 1>/dev/null
+if [ $? -eq 0 ]; then
+  patch -p0 -f ffmpeg/configure < ffmpeg_add_zlib_to_fontconfig_and_freetype.patch
+fi
+
 for i in "${SUPPORTED_ARCHITECTURES[@]}"
 do
   echo "Building for architecture $i"
