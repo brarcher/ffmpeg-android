5030028a529b9484f469f51f4f5bdb2a9938226d
Branden Archer <b.m.archer4@gmail.com>
2018-10-10 22:05:30 -0700

    Update fribidi to 1.0.5

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -12,7 +12,7 @@ rm -rf lame-*
 
 wget -O- https://download.sourceforge.net/libpng/libpng-1.6.35.tar.xz | tar xJ
 wget -O- https://github.com/libexpat/libexpat/releases/download/R_2_2_6/expat-2.2.6.tar.bz2 | tar xj
-wget -O- http://pkgs.fedoraproject.org/repo/pkgs/fribidi/fribidi-0.19.7.tar.bz2/6c7e7cfdd39c908f7ac619351c1c5c23/fribidi-0.19.7.tar.bz2 | tar xj
+wget -O- https://github.com/fribidi/fribidi/releases/download/v1.0.5/fribidi-1.0.5.tar.bz2 | tar xj
 wget -O- http://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz | tar xz
 echo "============================================"
 
