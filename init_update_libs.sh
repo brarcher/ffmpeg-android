9ba138b3c3b408ec4f88af1cdafc0c258175a895
Branden Archer <b.m.archer4@gmail.com>
2018-10-10 21:26:50 -0700

    Update libpng to 1.6.35

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -10,7 +10,7 @@ rm -rf expat-*
 rm -rf fribidi-*
 rm -rf lame-*
 
-wget -O- https://ftp.osuosl.org/pub/blfs/conglomeration/libpng/libpng-1.6.21.tar.xz | tar xJ
+wget -O- https://download.sourceforge.net/libpng/libpng-1.6.35.tar.xz | tar xJ
 wget -O- http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz | tar xz
 wget -O- http://pkgs.fedoraproject.org/repo/pkgs/fribidi/fribidi-0.19.7.tar.bz2/6c7e7cfdd39c908f7ac619351c1c5c23/fribidi-0.19.7.tar.bz2 | tar xj
 wget -O- http://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz | tar xz
